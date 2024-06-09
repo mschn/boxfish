6d1aff3e29bd0e69689eec242f6fbca0ab1b3870
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2024-06-09 22:24:57 +0200

    fix: improve routing

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ButtonModule],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  routes = [
+    {
+      name: 'Home',
+      link: '/',
+    },
+    {
+      name: 'Containers',
+      link: '/containers',
+    },
+  ];
+}

eecb7c9fbbf5ca77996b2108e8c9915ac20d7dac
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2024-06-09 18:21:32 +0200

    feat: add primeng table

--- ui/src/app/containers/containers.component.ts
+++ ui/src/app/containers/containers.component.ts
@@ -1,13 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { ContainerService } from '../services/container.service';
+import { TableModule } from 'primeng/table';
+import Dockerode from 'dockerode';
 
 @Component({
   selector: 'app-containers',
   standalone: true,
-  imports: [],
+  imports: [TableModule],
   templateUrl: './containers.component.html',
 })
 export class ContainersComponent {
   #containersService = inject(ContainerService);
   containers = this.#containersService.getContainers();
+
+  getContainerPorts(container: Dockerode.ContainerInfo): string {
+    const ports = container.Ports[0];
+    return ports ? `${ports.PublicPort}:${ports.PrivatePort}` : '';
+  }
 }

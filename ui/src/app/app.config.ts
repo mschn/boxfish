431444c4483fcd4a0981cbe1ce18861d9fa447c1
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2024-10-16 18:28:17 +0200

    refactor: use zoneless angular

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -10,7 +14,7 @@ import {
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
     provideQueryClient(

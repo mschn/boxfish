0abfc37c90e699af7261e5aef87340ac9e6f03c6
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2024-10-28 16:12:20 +0800

    feat: colored log output

--- ui/src/app/container/container-logs/container-logs.component.ts
+++ ui/src/app/container/container-logs/container-logs.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ContainerService } from '../../services/container.service';
 import { JsonPipe } from '@angular/common';
+import { AnsiUp } from 'ansi_up';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-container-logs',
@@ -10,5 +12,16 @@ import { JsonPipe } from '@angular/common';
   styleUrl: './container-logs.component.scss',
 })
 export class ContainerLogsComponent {
-  logs = inject(ContainerService).containerLogsFromRoute;
+  #containerService = inject(ContainerService);
+  #ansi_up = new AnsiUp();
+  #sanitizer = inject(DomSanitizer);
+
+  logs = this.#containerService.containerLogsFromRoute;
+  coloredLogs = computed(() => {
+    if (this.logs.isSuccess()) {
+      const html = this.#ansi_up.ansi_to_html(this.logs.data());
+      return this.#sanitizer.bypassSecurityTrustHtml(html);
+    }
+    return '';
+  });
 }

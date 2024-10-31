b71df078e88d9d67adae05d1cdf84c0541504327
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2024-10-31 14:05:05 +0800

    chore: improve server logging

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,17 @@ import { registerApi } from "./api";
 import { addCorsHook } from "./cors";
 import { Sessions } from "./session";
 
-const fastify = Fastify({ logger: true });
+const fastify = Fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+});
 addCorsHook(fastify);
 const sessions = new Sessions(fastify);
 registerApi(fastify, sessions);

f45a82c4bb7201f42c13e5397bbf27d2c8c7b882
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2024-10-31 17:44:45 +0800

    fix: active container tab style

--- ui/src/app/container/container.component.ts
+++ ui/src/app/container/container.component.ts
@@ -2,11 +2,18 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ContainerService } from '../services/container.service';
 import { StatusComponent } from './status.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, StatusComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    StatusComponent,
+    NgClass,
+  ],
   providers: [ContainerService],
   templateUrl: './container.component.html',
 })

961e5e2055c0c91959126b5f3a3233bacef4d207
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2024-12-21 19:37:39 +0100

    refactor: use register for /api prefix

--- server/src/api.ts
+++ server/src/api.ts
@@ -6,114 +6,121 @@ import { SESSION_ID } from "./model";
 import { Stream } from "stream";
 
 export function registerApi(fastify: FastifyInstance, sessions: Sessions) {
-  fastify.post("/api/login", async (request, reply) => {
-    const config = getDockerConfig();
-    const docker = new Docker(config);
-    const sessionId = createId();
-    sessions.add(sessionId, {
-      stats: {
-        lastUsed: new Date().getTime(),
-      },
-      docker,
-      config,
-    });
-    reply.cookie(SESSION_ID, sessionId, {
-      domain: "localhost",
-    });
-  });
-
-  fastify.get("/api/server", async (request, reply) => {
-    const session = sessions.fromContext(request);
-    return { config: session?.config, info: await session?.docker.info() };
-  });
-
-  fastify.get("/api/containers", async (request, reply) => {
-    const session = sessions.fromContext(request);
-    return await session?.docker.listContainers({ all: true });
-  });
-
-  fastify.get<{ Params: { id: string } }>(
-    "/api/containers/:id",
-    async (request, reply) => {
-      const id = request.params.id;
-      const session = sessions.fromContext(request);
-      const containers = await session?.docker.listContainers({ all: true });
-      return containers?.find((c) => c.Id === id);
-    }
-  );
-
-  fastify.put<{ Params: { id: string } }>(
-    "/api/containers/:id/stop",
-    async (request, reply) => {
-      const id = request.params.id;
-      const session = sessions.fromContext(request);
-      const container = session?.docker.getContainer(id);
-      await container?.stop();
-    }
-  );
-
-  fastify.put<{ Params: { id: string } }>(
-    "/api/containers/:id/start",
-    async (request, reply) => {
-      const id = request.params.id;
-      const session = sessions.fromContext(request);
-      const container = session?.docker.getContainer(id);
-      await container?.start();
-    }
-  );
-
-  fastify.get<{ Params: { id: string } }>(
-    "/api/containers/:id/logs",
-    async (request, reply) => {
-      const id = request.params.id;
-      const session = sessions.fromContext(request);
-      const container = session?.docker.getContainer(id);
-
-      var logStream = new Stream.PassThrough();
-      let output = "";
-      logStream.on("data", function (chunk) {
-        output += chunk.toString("utf8");
-      });
-
-      await new Promise<void>((resolve) => {
-        container?.logs(
-          {
-            follow: true,
-            stdout: true,
-            stderr: true,
-            tail: 1000,
+  fastify.register(
+    (app) => {
+      app.post("/login", async (request, reply) => {
+        const config = getDockerConfig();
+        const docker = new Docker(config);
+        const sessionId = createId();
+        sessions.add(sessionId, {
+          stats: {
+            lastUsed: new Date().getTime(),
           },
-          function (err, stream) {
-            if (err) {
-              reply.status(500).send(err);
-              resolve();
-            }
-            container.modem.demuxStream(stream, logStream, logStream);
-            stream?.on("end", function () {
-              logStream.end("");
-              reply.send(output);
-              resolve();
-            });
-
-            setTimeout(function () {
-              reply.send(output);
-              resolve();
-            }, 500);
-          }
-        );
+          docker,
+          config,
+        });
+        reply.cookie(SESSION_ID, sessionId, {
+          domain: "localhost",
+        });
       });
-    }
+
+      app.get("/server", async (request, reply) => {
+        const session = sessions.fromContext(request);
+        return { config: session?.config, info: await session?.docker.info() };
+      });
+
+      app.get("/containers", async (request, reply) => {
+        const session = sessions.fromContext(request);
+        return await session?.docker.listContainers({ all: true });
+      });
+
+      app.get<{ Params: { id: string } }>(
+        "/containers/:id",
+        async (request, reply) => {
+          const id = request.params.id;
+          const session = sessions.fromContext(request);
+          const containers = await session?.docker.listContainers({
+            all: true,
+          });
+          return containers?.find((c) => c.Id === id);
+        }
+      );
+
+      app.put<{ Params: { id: string } }>(
+        "/containers/:id/stop",
+        async (request, reply) => {
+          const id = request.params.id;
+          const session = sessions.fromContext(request);
+          const container = session?.docker.getContainer(id);
+          await container?.stop();
+        }
+      );
+
+      app.put<{ Params: { id: string } }>(
+        "/containers/:id/start",
+        async (request, reply) => {
+          const id = request.params.id;
+          const session = sessions.fromContext(request);
+          const container = session?.docker.getContainer(id);
+          await container?.start();
+        }
+      );
+
+      app.get<{ Params: { id: string } }>(
+        "/containers/:id/logs",
+        async (request, reply) => {
+          const id = request.params.id;
+          const session = sessions.fromContext(request);
+          const container = session?.docker.getContainer(id);
+
+          var logStream = new Stream.PassThrough();
+          let output = "";
+          logStream.on("data", function (chunk) {
+            output += chunk.toString("utf8");
+          });
+
+          await new Promise<void>((resolve) => {
+            container?.logs(
+              {
+                follow: true,
+                stdout: true,
+                stderr: true,
+                tail: 1000,
+              },
+              function (err, stream) {
+                if (err) {
+                  reply.status(500).send(err);
+                  resolve();
+                }
+                container.modem.demuxStream(stream, logStream, logStream);
+                stream?.on("end", function () {
+                  logStream.end("");
+                  reply.send(output);
+                  resolve();
+                });
+
+                setTimeout(function () {
+                  reply.send(output);
+                  resolve();
+                }, 500);
+              }
+            );
+          });
+        }
+      );
+
+      app.get("/images", async (request, reply) => {
+        const session = sessions.fromContext(request);
+        return await session?.docker.listImages({ all: true });
+      });
+
+      app.get("/volumes", async (request, reply) => {
+        const session = sessions.fromContext(request);
+        return (await session?.docker.listVolumes())?.Volumes;
+      });
+    },
+    { prefix: "/api" }
   );
-
-  fastify.get("/api/images", async (request, reply) => {
-    const session = sessions.fromContext(request);
-    return await session?.docker.listImages({ all: true });
-  });
-
-  fastify.get("/api/volumes", async (request, reply) => {
-    const session = sessions.fromContext(request);
-    return (await session?.docker.listVolumes())?.Volumes;
-  });
 }
 
 function getDockerConfig(): Docker.DockerOptions {

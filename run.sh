1f9791c1472ef2a99e6440d8a753f77cee094030
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2024-12-22 15:24:01 +0100

    feat: auto detect colima docker sock

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock -p 3000:3000 boxfish
+SOCK="/var/run/docker.sock"
+
+if [ -e "~/.colima/docker.sock" ]; then
+    SOCK="~/.colima/docker.sock"
+fi
+
+docker run \
+    -v ${SOCK}:/var/run/docker.sock \
+    -p 3000:3000 \
+    boxfish -d

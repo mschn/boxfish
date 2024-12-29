b05c357cebc1bef839aabd7312ccba0de874fb6a
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2024-12-29 19:57:55 +0100

    fix: remove table borders

--- ui/src/app/containers/containers-placeholder.component.ts
+++ ui/src/app/containers/containers-placeholder.component.ts
@@ -3,31 +3,29 @@ import { SkeletonModule } from 'primeng/skeleton';
 import { TableModule } from 'primeng/table';
 
 @Component({
-    selector: 'app-containers-placeholder',
-    template: ` <div class="border border-gray-200 dark:border-gray-700">
-    <p-table [value]="[{}, {}, {}, {}]">
-      <ng-template pTemplate="header">
-        <tr>
-          <th i18n>Name</th>
-          <th i18n>Image</th>
-          <th i18n>Stack</th>
-          <th i18n>Status</th>
-          <th i18n>Ports</th>
-          <th i18n>Actions</th>
-        </tr>
-      </ng-template>
-      <ng-template pTemplate="body" let-container>
-        <tr>
-          <td><p-skeleton></p-skeleton></td>
-          <td><p-skeleton></p-skeleton></td>
-          <td><p-skeleton></p-skeleton></td>
-          <td><p-skeleton></p-skeleton></td>
-          <td><p-skeleton></p-skeleton></td>
-          <td><p-skeleton></p-skeleton></td>
-        </tr>
-      </ng-template>
-    </p-table>
-  </div>`,
-    imports: [TableModule, SkeletonModule]
+  selector: 'app-containers-placeholder',
+  template: `<p-table [value]="[{}, {}, {}, {}]">
+    <ng-template pTemplate="header">
+      <tr>
+        <th i18n>Name</th>
+        <th i18n>Image</th>
+        <th i18n>Stack</th>
+        <th i18n>Status</th>
+        <th i18n>Ports</th>
+        <th i18n>Actions</th>
+      </tr>
+    </ng-template>
+    <ng-template pTemplate="body" let-container>
+      <tr>
+        <td><p-skeleton></p-skeleton></td>
+        <td><p-skeleton></p-skeleton></td>
+        <td><p-skeleton></p-skeleton></td>
+        <td><p-skeleton></p-skeleton></td>
+        <td><p-skeleton></p-skeleton></td>
+        <td><p-skeleton></p-skeleton></td>
+      </tr>
+    </ng-template>
+  </p-table>`,
+  imports: [TableModule, SkeletonModule],
 })
 export class ContainersPlaceholderComponent {}
--- ui/src/app/images/images-placeholder.component.ts
+++ ui/src/app/images/images-placeholder.component.ts
@@ -5,29 +5,27 @@ import { TableModule } from 'primeng/table';
 @Component({
   selector: 'app-images-placeholder',
   imports: [SkeletonModule, TableModule],
-  template: ` <div class="border border-gray-200 dark:border-gray-700">
-    <p-table [value]="[{}, {}, {}, {}]">
-      <ng-template pTemplate="header">
-        <tr>
-          <th i18n>Name</th>
-          <th i18n>Version</th>
-          <th i18n>ID</th>
-          <th i18n>Created</th>
-          <th i18n>Size</th>
-          <th i18n>Actions</th>
-        </tr>
-      </ng-template>
-      <ng-template pTemplate="body" let-image>
-        <tr>
-          <td><p-skeleton></p-skeleton></td>
-          <td><p-skeleton></p-skeleton></td>
-          <td><p-skeleton></p-skeleton></td>
-          <td><p-skeleton></p-skeleton></td>
-          <td><p-skeleton></p-skeleton></td>
-          <td><p-skeleton></p-skeleton></td>
-        </tr>
-      </ng-template>
-    </p-table>
-  </div>`,
+  template: `<p-table [value]="[{}, {}, {}, {}]">
+    <ng-template pTemplate="header">
+      <tr>
+        <th i18n>Name</th>
+        <th i18n>Version</th>
+        <th i18n>ID</th>
+        <th i18n>Created</th>
+        <th i18n>Size</th>
+        <th i18n>Actions</th>
+      </tr>
+    </ng-template>
+    <ng-template pTemplate="body" let-image>
+      <tr>
+        <td><p-skeleton></p-skeleton></td>
+        <td><p-skeleton></p-skeleton></td>
+        <td><p-skeleton></p-skeleton></td>
+        <td><p-skeleton></p-skeleton></td>
+        <td><p-skeleton></p-skeleton></td>
+        <td><p-skeleton></p-skeleton></td>
+      </tr>
+    </ng-template>
+  </p-table>`,
 })
 export class ImagesPlaceholderComponent {}

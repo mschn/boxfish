6092b053149a7a287d0debdf84ae76a857f7ca18
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2025-01-04 23:39:52 +0100

    test: add e2e for lang selection (#14)

--- e2e/pages/app.page.ts
+++ e2e/pages/app.page.ts
@@ -1,15 +1,28 @@
 import { Locator, Page } from "@playwright/test";
 import { ContainersPage } from "./containers.page";
+import { SettingsPage } from "./settings.page";
 
 export class AppPage {
   containersLink: Locator;
+  settingsLink: Locator;
 
   constructor(readonly page: Page) {
     this.containersLink = this.page.getByRole("link", { name: "Containers" });
+    this.settingsLink = this.page.getByRole("link", { name: "Settings" });
   }
 
   async openContainers(): Promise<ContainersPage> {
     await this.containersLink.click();
     return new ContainersPage(this.page);
   }
+
+  async openSettingsPage(): Promise<SettingsPage> {
+    await this.settingsLink.click();
+    return new SettingsPage(this.page);
+  }
+}
+
+export async function openApp(page: Page) {
+  await page.goto("http://localhost:3000/");
+  return new AppPage(page);
 }

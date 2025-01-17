d959c705239569136469c6d7cac445b478cc6eee
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2025-01-17 22:03:25 +0100

    feat: move scrollbar into each sub component

--- ui/src/app/containers/container/container-logs/container-logs-placeholder.component.ts
+++ ui/src/app/containers/container/container-logs/container-logs-placeholder.component.ts
@@ -5,13 +5,16 @@ import { SkeletonModule } from 'primeng/skeleton';
   selector: 'app-container-logs-placeholder',
   template: `
     <div
-      class="flex flex-col gap-3
+      class="w-full flex flex-col gap-3
    bg-gray-100 dark:bg-gray-800 dark:text-gray-100 
-    mt-3 rounded-md border border-gray-200 dark:border-gray-700"
+    rounded-md border border-gray-200 dark:border-gray-700"
     >
       <p-skeleton width="100%" height="20rem"></p-skeleton>
     </div>
   `,
+  host: {
+    class: 'w-full',
+  },
   imports: [SkeletonModule],
 })
 export class ContainerLogsPlaceholderComponent {}

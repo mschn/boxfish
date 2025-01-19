133dd44efa7836a581671ac123777da594782149
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2025-01-19 15:33:57 +0100

    build: validate-i18n prefills missing keys

--- ui/scripts/validate-i18n.js
+++ ui/scripts/validate-i18n.js
@@ -1,19 +1,45 @@
-const en = require('../src/locale/messages.json');
-const fr = require('../src/locale/messages.fr.json');
+const { writeFileSync } = require("fs");
+const en = require("../src/locale/messages.json");
+const path = require("path");
 
-const errors = [];
+const langs = ["fr"];
 const keys = Object.keys(en.translations);
-for (const key of keys) {
-  if (!Object.hasOwn(fr.translations, key)) {
-    errors.push(`Error: missing FR translation for "${key}" = "${en.translations[key]}"`);
+const allErrors = [];
+
+for (const lang of langs) {
+  const messagesPath = path.resolve(
+    __dirname,
+    `../src/locale/messages.${lang}.json`,
+  );
+  const messages = require(messagesPath);
+  const updatedTranslations = {};
+  const errors = [];
+
+  for (const key of keys) {
+    if (
+      !Object.hasOwn(messages.translations, key) ||
+      messages.translations[key] === null
+    ) {
+      errors.push(
+        `Error: missing ${lang} translation for "${key}" = "${en.translations[key]}"`,
+      );
+      updatedTranslations[key] = null;
+    } else {
+      updatedTranslations[key] = messages.translations[key];
+    }
+  }
+
+  if (errors.length > 0) {
+    messages.translations = updatedTranslations;
+    writeFileSync(messagesPath, JSON.stringify(messages, null, 2));
+    allErrors.push(...errors);
   }
 }
 
-console.log('validate-i18n.js:')
-console.error(errors.join('\n'));
-if (errors.length > 0) {
+console.log("validate-i18n.js:");
+console.error(allErrors.join("\n"));
+if (allErrors.length > 0) {
   process.exitCode = 1;
 } else {
   console.log(`Successfully validated ${keys.length} messages.`);
 }
-

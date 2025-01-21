c88d68e2043dd077a13f459e609d4df7e7352e34
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2025-01-21 20:57:08 +0100

    fix: minor padding in container views

--- ui/src/app/containers/container/container-logs/container-logs-placeholder.component.ts
+++ ui/src/app/containers/container/container-logs/container-logs-placeholder.component.ts
@@ -6,9 +6,8 @@ import { SkeletonModule } from 'primeng/skeleton';
   template: `
     <div
       class="w-full flex flex-col gap-3
-   bg-gray-100 dark:bg-gray-800 dark:text-gray-100 
-    rounded-md border border-gray-200 dark:border-gray-700"
-      data-testid="contaier-logs-placeholder"
+   bg-gray-100 dark:bg-gray-800 dark:text-gray-100"
+      data-testid="container-logs-placeholder"
     >
       <p-skeleton width="100%" height="20rem"></p-skeleton>
     </div>

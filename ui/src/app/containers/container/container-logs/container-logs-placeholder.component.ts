ef457a928034ee9449dbcd8fdd9f194bf60b13f8
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2025-01-21 16:52:21 +0100

    feat: container logs empty state

--- ui/src/app/containers/container/container-logs/container-logs-placeholder.component.ts
+++ ui/src/app/containers/container/container-logs/container-logs-placeholder.component.ts
@@ -8,6 +8,7 @@ import { SkeletonModule } from 'primeng/skeleton';
       class="w-full flex flex-col gap-3
    bg-gray-100 dark:bg-gray-800 dark:text-gray-100 
     rounded-md border border-gray-200 dark:border-gray-700"
+      data-testid="contaier-logs-placeholder"
     >
       <p-skeleton width="100%" height="20rem"></p-skeleton>
     </div>
